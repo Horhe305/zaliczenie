f0c32f5247be73cbe104eae6f465406d577d8a00
Horhe305 <101187054+Horhe305@users.noreply.github.com>
2022-06-21 10:40:22 +0000

    koniec

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ class Main
   public static void main(String[] args)
   {
     Scanner scan = new Scanner(System.in);
-    System.out.println("");
-  }
+    int tab[] = {1, 2, 3, 4, 5, 6, 7, 8};
+     System.out.println("Posortowana malejaco:");
+    for (int i=0; i<tab.length; i++)
+      {
+        System.out.print(tab[(tab.length)-(i+1)]);
+        System.out.print(" ");
+      }
+}
 }
